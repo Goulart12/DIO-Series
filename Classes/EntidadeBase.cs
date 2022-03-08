5c9373f2d25785f26a4d40d61d482f1afb9fda1e
Juarez Goulart <goulartgarcia12@gmail.com>
2022-03-08 00:17:26 -0300

    version 1.0

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,6 @@
+﻿namespace DIO_Series;
+
+public abstract class EntidadeBase
+{
+	public int Id { get; protected set; }
+}
---
+++ Classes/Repositorio.cs
@@ -0,0 +1,38 @@
+﻿using DIO_Series.Interfaces;
+
+namespace DIO_Series;
+
+public class Repositorio : IRepositorio<Serie>
+{
+	private List<Serie> listaSerie = new List<Serie>();
+	
+	public List<Serie> Lista()
+	{
+		return listaSerie;
+	}
+
+	public Serie RetornaPorId(int id)
+	{
+		return listaSerie[id];
+	}
+
+	public void Insere(Serie entidade)
+	{
+		listaSerie.Add(entidade);
+	}
+
+	public void Exclui(int id)
+	{
+		listaSerie[id].Excluir();
+	}
+
+	public void Atualiza(int id, Serie entidade)
+	{
+		listaSerie[id] = entidade;
+	}
+
+	public int ProximoId()
+	{
+		return listaSerie.Count;
+	}
+}
